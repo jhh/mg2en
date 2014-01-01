fb7504d17bf448d6d1e8f25ca0891d551a28b11a
Jeff Hutchison <jeff@jeffhutchison.com>
2014-01-01 14:44:17 -0500

    Parse ingredient groups

--- lib/mg2en/ingredient.rb
+++ lib/mg2en/ingredient.rb
@@ -1,18 +1,31 @@
 module Mg2en
 
   class Ingredient
-    attr_reader :measurement, :quantity, :description, :direction
+    attr_reader :measurement, :quantity, :description, :direction, :group, :ingredients
+    alias_method :group?, :group
 
     def initialize(i)
-      @quantity    = i['QUANTITY']
-      @measurement = i['MEASUREMENT']
-      @description = i['DESCRIPTION']
-      @direction   = i['DIRECTION']
+      if i.has_key?('DESCRIPTION')
+        @quantity    = i['QUANTITY']
+        @measurement = i['MEASUREMENT']
+        @description = i['DESCRIPTION']
+        @direction   = i['DIRECTION']
+        @group       = i['IS_DIVIDER']
+      elsif i.has_key?('DIVIDER_INGREDIENT')
+        @group       = true
+        @ingredients = Array.new
+        ingts = i['INGREDIENTS']
+        ingts.each do |ing|
+          ingredient = Mg2en::Ingredient.new(ing)
+          @ingredients.push ingredient
+        end
+      else
+        raise RuntimeError
+      end
     end
 
     def enml
       "#{self.quantity} #{self.measurement} #{self.description}, #{self.direction}"
     end
   end
-
 end
--- spec/ingredient_spec.rb
+++ spec/ingredient_spec.rb
@@ -5,13 +5,26 @@ describe Mg2en::Ingredient do
   context "with valid input" do
     let(:recipes) { Mg2en::parse_xml(File.dirname(__FILE__) + '/fixtures/1.mgourmet3') }
 
-    it "parses top-level ingredients" do
+    it "parses ingredients" do
       i = recipes[0].ingredients
+      expect(i.size).to eql(2)
       expect(i[0].measurement).to eql("clove")
       expect(i[0].quantity).to eql("1")
       expect(i[0].description).to eql("Garlic")
       expect(i[0].direction).to eql("")
       expect(i[1].direction).to eql("chopped")
+      expect(i[0].group?).to be false
+    end
+
+    it "parses ingredient groups" do
+      i = recipes[1].ingredients
+      expect(i.size).to eql(5)
+      expect(i[0].group?).to be false
+      ig = i[2]
+      expect(ig.group?).to be true
+      expect(ig.ingredients.size).to eql(2)
+      expect(ig.ingredients[0].description).to eql("saffron")
+      expect(ig.ingredients[1].description).to eql("salt")
     end
   end
 end
